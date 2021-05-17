2a7c4b00faf240087aa6f5e88515ca43df0dde6a
Ali Rostami <alirostami.1313@gmail.com>
2021-05-17 16:44:33 +0430

    initial commit, chapter 3 Completed

---
+++ cmd/web/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// server mux routes
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	// Static fileserving from ./ui/static
+	fileserver := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
+	// start the server
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AliRostami1/snippetbox
+
+go 1.16
